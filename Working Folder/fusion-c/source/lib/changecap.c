1679c478d311fb98aeea8ee6ca15a30b7f3b36f8
sylvain <sylvain@sylvain-ProDesk>
2019-08-24 18:43:08 +0200

    changecap.c compilation warning fixed

--- Working Folder/fusion-c/source/lib/changecap.c
+++ Working Folder/fusion-c/source/lib/changecap.c
@@ -8,7 +8,7 @@
 |             |_|  \__,_|___/_|\___/|_| |_| *               |
 |                                                           |
 |               The MSX C Library for SDCC                  |
-|                   V1.0 - 09-10-11 2018                    |
+|                   V1.2 - 08 2019                          |
 |                                                           |
 |                Eric Boez &  Fernando Garcia               |
 |                                                           |
@@ -25,19 +25,19 @@
 // Change Capital Led of the keyboard On/off
 void ChangeCap(char n) __naked
 {
-     __asm
-        push ix
-       
-        ld   ix,#0
-        add  ix,sp
-        ld   a,4(ix)
-        push iy
-        ld   ix,#0x0132       ; Bios  routine
-        ld   iy,(0xFCC0)      ; mainrom slotaddress
-        call 0x001c           ; interslotcall
-        ei
-        pop IY
-        pop IX
-        ret 
-        __endasm;
+n;
+  __asm
+  push ix
+  ld   ix,#0
+  add  ix,sp
+  ld   a,4(ix)
+  push iy
+  ld   ix,#0x0132       ; Bios  routine
+  ld   iy,(0xFCC0)      ; mainrom slotaddress
+  call 0x001c           ; interslotcall
+  ei
+  pop  iy
+  pop  ix
+  ret
+  __endasm;
 }
