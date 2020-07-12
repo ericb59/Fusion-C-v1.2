1614932bbc4bc50d1c0695e95f25547b9abf91ae
Snakek <snakek@Snakek-Debian>
2020-07-12 14:52:47 -0300

    Linux Patch

--- Working Folder/openMSX/emul_start.sh
+++ Working Folder/openMSX/emul_start.sh
@@ -1,13 +1,30 @@
-#!/bin/bash	
+#!/bin/bash
 # OpenMSX Start Script
 # The emulator will be started only if it's not already active
 
+#MacOS
+if [ "$(uname -s)" == "Darwin" ]; then
+	xopenmsx=`ps x | grep "openmsx " | grep -v grep`
+	 
+	if [ "${xopenmsx}" == "" ]; then
+	  echo "...Now Starting OpenMsx "
+	  ./openMSX/openMSX.app/Contents/MacOS/openmsx -script ./openMSX/emul_start_config.txt | NULL
+	else
+	  echo "...openMSX already running "
+	fi
 
-xopenmsx=`ps x | grep "openmsx " | grep -v grep`
- 
-if [ "${xopenmsx}" == "" ]; then
-  echo "...Now Starting OpenMsx "
-  ./openMSX/openMSX.app/Contents/MacOS/openmsx -script ./openMSX/emul_start_config.txt | NULL
+#Linux
+elif [ "$(uname -s)" == "Linux" ]; then
+	xopenmsx=`pgrep "openmsx"`
+	
+	if [ "${xopenmsx}" == "" ]; then
+		echo "...Now Starting OpenMSX "
+		/opt/openMSX/bin/openmsx -script ./openMSX/emul_start_config.txt
+	else
+		echo "...openMSX already running "
+	fi
+
+#Other
 else
-  echo "...openMSX already running "
+	echo "Failed to identify OS"
 fi
--- Working Folder/openMSX/readme.txt
+++ Working Folder/openMSX/readme.txt
@@ -1,1 +1,2 @@
-Copy here the OPenMSX Executable. Previously set with needed system roms.
+MacOS and Windows: Copy here the OpenMSX Executable. Previously set with needed system roms.
+Linux: This script assumes you've compiled the emulator with make install, which puts the executable in /opt/openMSX/bin. Change if needed.
